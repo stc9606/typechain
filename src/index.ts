6bae5b1df8bcddc13fb55757b4d23d32f4f753ab
노승철 <s_chuli@noseungcheol-ui-MacBookAir.local>
2021-02-26 16:49:09 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-const name = "Seung Chul",
-    age = 26,
-    gender = "male"
+import * as CryptoJS from 'crypto-js'; 
 
-const info = (name: string, age: number, gender: string) : string => {
-    return `Hi ${name}, You are ${age}, You Are a ${gender}!`;
+class Block {
+    public index : number;
+    public hash : string;
+    public previousHash : string;
+    public data : string;
+    public timestamp : number;
+
+    static calculateBlockHash = (index: number, previousHash:string, data: string, timestamp: number):string => {
+        return CryptoJS.SHA256(index + previousHash + data + timestamp);
+    }
+
+    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-console.log(info(name, age, gender));
+const genesisBlock : Block = new Block(0, "20202020", "", "Hello", 123456);
+
+let blockchain : Block[] = [genesisBlock];
+
+const getBlockchain = () :Block[] => blockchain;
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+console.log(blockchain);
 
 export {};
